c712c89067acb8408956acd098ad01345f83da5e
hmalik144 <h.malik144@gmail.com>
2019-10-01 14:18:08 +1000

    Update Skype.ps1

--- Skype.ps1
+++ Skype.ps1
@@ -1,5 +1,5 @@
 Disable-csuser userid
 Enable-CsUser userid -RegistrarPool "lncpool01.internal.*****.gov.au" -SipAddressType EmailAddress
-set-csuser userid -LineURI "tel:+6129210xxxx;ext=xxxx" -EnterpriseVoiceEnabled $true
+set-csuser userid -LineURI "tel:+61*****xxxx;ext=xxxx" -EnterpriseVoiceEnabled $true
 Grant-csVoicePolicy -identity userid -policyname AU-SYD-VP
 Grant-csdialplan -identity userid -policyname AU-SYD-DP
